d540994a76865e15cb94ddeb3ff831351c62a354
Jake Armstrong <15316168+rhit-armstrjj@users.noreply.github.com>
2022-12-13 19:34:01 -0500

    Created Step File for HDMI to CSI

---
+++ ansible/buzzer/README.md
@@ -0,0 +1,5 @@
+# How To Understand Audio Cues?
+1. Ascending Major Scale -> (Startup -> All Healthy)
+2. Descending Major Scale -> Graceful Shutdown
+2. 3 Long Beeps -> A process has stopped
+3. Perfect Fifth -> A process has restarted
--- ansible/buzzer/__main__.py
+++ ansible/buzzer/__main__.py
@@ -1,2 +1,37 @@
 # TODO Buzzer Service that listens to SystemD and offers
 # Signals that can be used.
+
+# Mido for midi tones
+import signal
+import os
+import gpiozero.TonalBuzzer as Buzzer
+from gpiozero.tones import Tone
+
+class ShutdownGuard:
+    """Allows for the buzzer to play a shutdown tone"""
+    end_process = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.graceful_exit)
+
+
+    def graceful_exit(self):
+        self.end_process = True
+
+class BuzzerController:
+    """Controls and manages sounds for the buzzer.
+       Once queue is done
+    """
+    def __init__(self, pin: int, guard: ShutdownGuard):
+        self.guard = guard
+        self.buzzer = Buzzer()
+        self.tones = [] # A queue of (tone, duration) unions to play 
+
+    def add_to_queue(self, list):
+        self.tones.append(list)
+
+    
+
+if __name__ == "__main__":
+    guard = ShutdownGuard()
+    controller = BuzzerController(33, guard)
