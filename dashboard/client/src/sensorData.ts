253ef18eaa063491da5eb727bae75690d2b13871
Evelyn Gordon <evelyn.gordon20@gmail.com>
2023-02-16 18:14:48 -0500

    Move dashboard to groundstation

--- dashboard/client/src/sensorData.ts
+++
@@ -1,8 +0,0 @@
-import type { ShallowRef } from "vue"
-
-export type SensorData = {
-  "id": string,
-  "packets": ShallowRef<any[]>
-}
-
-export type ChannelType = "vector" | "number" | "boolean" | "string" | "unknown"
