c54be0465063cf670d0639c3b4b07dee6d195dd0
Evelyn Gordon <evelyn.gordon20@gmail.com>
2023-02-22 17:30:45 -0500

    Improve system graphs

--- ansible/sensord/src/reader/sensors/system.py
+++ ansible/sensord/src/reader/sensors/system.py
@@ -2,6 +2,8 @@ from .sensor import PollingSensor
 from pathlib import Path
 import psutil
 import paho.mqtt.client as mqtt
+from humanize import naturalsize
+
 
 class SystemSensor(PollingSensor):
     def __init__(self, client: mqtt.Client) -> None:
@@ -22,8 +24,9 @@ class SystemSensor(PollingSensor):
                 },
                 {
                     "key": "cpu_usage",
-                    "name": "CPU Usage",
-                    "type": "number",
+                    "name": f"CPU Usage ({psutil.cpu_count()} cores)",
+                    "type": "vector",
+                    "components": [f"core {i}" for i in range(psutil.cpu_count())],
                     "unit": "%",
                     "minimum": 0,
                     "maximum": 100,
@@ -39,15 +42,11 @@ class SystemSensor(PollingSensor):
                 {
                     "key": "mem_usage",
                     "name": "Memory Usage",
-                    "type": "number",
-                    "unit": "%",
-                    "minimum": 0,
-                    "maximum": 100,
-                },
-                {
-                    "key": "swap_usage",
-                    "name": "Swap Usage",
-                    "type": "number",
+                    "type": "vector",
+                    "components": (
+                        f"memory ({naturalsize(psutil.virtual_memory().total, True)})",
+                        f"swap ({naturalsize(psutil.swap_memory().total, True)})",
+                    ),
                     "unit": "%",
                     "minimum": 0,
                     "maximum": 100,
@@ -67,7 +66,7 @@ class SystemSensor(PollingSensor):
         return 0.5
 
     def _poll(self) -> None:
-        cpu_usage = psutil.cpu_percent()
+        cpu_usage = psutil.cpu_percent(percpu=True)
         cpu_frequency = psutil.cpu_freq().current
         memory_usage = psutil.virtual_memory().percent
         swap_usage = psutil.swap_memory().percent
@@ -76,14 +75,14 @@ class SystemSensor(PollingSensor):
         cpu_temperature = None
         temperature_data = psutil.sensors_temperatures()
         if "cpu_thermal" in temperature_data:
-            cpu_temperature = temperature_data['cpu_thermal'][0].current
+            cpu_temperature = temperature_data["cpu_thermal"][0].current
 
         self.publish(
             {
                 "cpu_temp": cpu_temperature,
                 "cpu_usage": cpu_usage,
                 "cpu_freq": cpu_frequency,
-                "mem_usage": memory_usage,
+                "mem_usage": (memory_usage, swap_usage),
                 "swap_usage": swap_usage,
                 "disk_usage": disk_usage,
             },
