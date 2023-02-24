8f58bde3d8cc53042839c6a48fa88f55b8ebf974
Evelyn Gordon <evelyn.gordon20@gmail.com>
2023-02-23 23:25:47 -0500

    Calibrate battery voltage

--- ansible/sensord/src/reader/sensors/mcp3004.py
+++ ansible/sensord/src/reader/sensors/mcp3004.py
@@ -3,13 +3,14 @@ import iio
 import os
 
 # Calculated experientially, regression on y = k*x
-LSB_VALUE = 1/27.6982857142857
+LSB_VALUE = 1/26.875
 MINIMUM_CELL_VOLTAGE = 3.3
 MAXIMUM_CELL_VOLTAGE = 4.2
 CELLS_SERIES = 2
 MINIMUM_VOLTAGE = MINIMUM_CELL_VOLTAGE * CELLS_SERIES
 MAXIMUM_VOLTAGE = MAXIMUM_CELL_VOLTAGE * CELLS_SERIES
 CONSECUTIVE_UNDERVOLTAGE_SHUTDOWN = 10
+CHANNEL_NAME = "voltage0"
 
 class MCP3004Sensor(PollingSensor):
     def _get_sensor_id(self) -> str:
@@ -35,7 +36,7 @@ class MCP3004Sensor(PollingSensor):
 
     def _run(self) -> None:
         self.dev = iio.Context().find_device("mcp3008")
-        self.ch = self.dev.find_channel("voltage0")
+        self.ch = self.dev.find_channel(CHANNEL_NAME)
         self.consecutive_undervoltages = 0
 
         return super()._run()
@@ -44,11 +45,12 @@ class MCP3004Sensor(PollingSensor):
         voltage = int(self.ch.attrs["raw"].value) * LSB_VALUE
 
         if voltage < MINIMUM_VOLTAGE:
-            print(f"WARNING: BATTERY VOLTAGE OF {voltage:.2f}V IS BELOW MINIMUM {MINIMUM_VOLTAGE:.2f}V")
+            self._logger.warn(f"WARNING: BATTERY VOLTAGE OF {voltage:.2f}V IS BELOW MINIMUM {MINIMUM_VOLTAGE:.2f}V")
             self.consecutive_undervoltages += 1
             if self.consecutive_undervoltages > CONSECUTIVE_UNDERVOLTAGE_SHUTDOWN:
-                print("SHUTTING DOWN SYSTEM")
+                self._logger.critical("SHUTTING DOWN SYSTEM")
                 os.system("shutdown now")
+                exit(0)
         else:
             self.consecutive_undervoltages = 0
 
